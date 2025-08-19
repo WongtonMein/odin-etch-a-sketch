daa9201f7e348b07e7c6c880d786c717a83f2786
WongtonMein <190700700+WongtonMein@users.noreply.github.com>
2025-08-19 15:05:55 -0700

    Black and RGB functionality, create grid and reset buttons

--- javascript.js
+++ javascript.js
@@ -1,62 +1,75 @@
-let mainGrid = document.getElementById("container");
-let currentMode = "classic"
-const defaultGridLength = 10;
+// window dims
 const viewportWidth = window.innerWidth;
 const viewportHeight = window.innerHeight;
 let gridLength
 let cellSize
 
+// sidebar buttons
 let bttnDrawGrid = document.getElementById("create-grid");
 let bttnClassic = document.getElementById("classic");
 let bttnRGB = document.getElementById("rgb");
 let bttnGradient = document.getElementById("gradient");
 let bttnReset = document.getElementById("reset");
 
-function setMode(mode) {
-    switch(mode) {
-        case "classic":
-            playGrid();
-            break;
-        case "rgb":
-            playRGB();
-            break;
-        case "gradient":
-            playGradient();
-            break;
-    }
-};
+// default settings
+let gridContainer = document.getElementById("container");
+let currentMode = "black"
+const defaultGridLength = 10;
+bttnClassic.style.background = "gainsboro";
+const opacityIncrease = 0.1
+
+// change cell draw color
+bttnClassic.addEventListener("click", () => {
+    currentMode = "black";
+    bttnClassic.style.background = "gainsboro";
+    bttnRGB.style.background = "white";
+    bttnGradient.style.background = "white";
+    console.log(currentMode);});
+bttnRGB.addEventListener("click", () => {
+    currentMode = "rgb";
+    bttnClassic.style.background = "white";
+    bttnRGB.style.background = "gainsboro";
+    bttnGradient.style.background = "white";
+    console.log(currentMode);}); 
+bttnGradient.addEventListener("click", () => {
+    // implement gradient function
+    currentMode = "gradient";
+    bttnClassic.style.background = "white";
+    bttnRGB.style.background = "white";
+    bttnGradient.style.background = "gainsboro";
+    console.log(currentMode);}); 
 
 function setGridLength() {
     let gridLength = defaultGridLength
     do {
         let input = prompt("How many squares do you want per side? (max 100)")
-        gridLength = parseInt(input)
+        gridLength = parseFloat(input)
     } while (gridLength < 1 || gridLength > 100 || isNaN(gridLength));
     return gridLength
 };
 
 function setCellSize() {
     if (viewportWidth <= viewportHeight) {
-        return parseInt((viewportWidth - 200) / gridLength)
+        return parseFloat((viewportWidth - 200) / gridLength)
     } else {
-        return parseInt((viewportHeight - 200) / gridLength)
+        return parseFloat((viewportHeight - 200) / gridLength)
     };
 };
 
 function drawContainer() {
-    let mainGridLength
+    let gridContainerLength
     if (viewportWidth <= viewportHeight) {
-        mainGridLength = parseInt(viewportWidth - 200)
+        gridContainerLength = parseInt(viewportWidth - 200)
     } else {
-        mainGridLength = parseInt(viewportHeight - 200)
+        gridContainerLength = parseInt(viewportHeight - 200)
     }
-    mainGrid.style.width = mainGridLength + "px";
-    mainGrid.style.height = mainGridLength + "px";
-    mainGrid.style.border = "1px solid black";
-}
+    gridContainer.style.minWidth = gridContainerLength + "px";
+    gridContainer.style.minHeight = gridContainerLength + "px";
+    gridContainer.style.border = "1px solid black";
+};
 
 function drawGrid(gridLength, cellSize) {
-    mainGrid.replaceChildren()
+    gridContainer.replaceChildren()
     for (i = 0; i < gridLength; i++) {
         let column = document.createElement("div");
         for (j = 0; j < gridLength; j++) {
@@ -66,36 +79,56 @@ function drawGrid(gridLength, cellSize) {
             cell.style.width = cellSize + "px";
             cell.style.height = cellSize + "px";
             column.appendChild(cell);
-        }
-        mainGrid.appendChild(column);
-}};
 
-// function highlightCell() {
-//     let divCell = document.querySelector("div.cell");
-//     divCell.classList.add("cellHighlight");
+            // figure out how to break off into separate function
+            cell.addEventListener("mouseover", () => {
+                if (currentMode === "black") {
+                    cell.style.backgroundColor = "black";
+                } else if (currentMode === "rgb") {
+                    cell.style.backgroundColor = generateRandomColor();
+                // } else if (currentMode === "gradient") {
+                    // cell.style.backgroundColor = increaseCellOpacity(cell);
+                }
+
+            // reset
+            bttnReset.addEventListener("click", () => {
+                cell.style.backgroundColor = "white";
+            }
+            );
+            });
+        };
+        gridContainer.appendChild(column);
+    };
+};
+
+// extra credit
+function generateRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`
+};
+
+function increaseCellOpacity(cell) {
+    let currentOpacity = parseFloat(window.getComputedStyle(cell).opacity);
+    let newOpacity = Math.min(currentOpacity + opacityIncrease, 1.0);
     
-//     let cellHighlight = document.querySelector("cellHighlight");
-//     cellHighlight.addEventListener("mouseover", () => {
-//         cellHighlight.style.backgroundColor = "black";
-//     })
-// };
+    cell.setAttribute("style", `background-color: rgb(255, 255, 255, ${newOpacity}`)
+};
 
 
-// add function to set and draw container first
-drawContainer()
-bttnDrawGrid.addEventListener("click", () => {
+drawContainer(); // functional
+bttnDrawGrid.addEventListener("click", () => { // functional
     gridLength = setGridLength();
     cellSize = setCellSize();
     drawGrid(gridLength, cellSize);
-}); // functional
-// highlightCell();
-    
-
+}); 
 
 // dynamically resizes grid based on window size
 // need to implement keep grid background color
     // function redrawGrid() {
-    //     mainGrid.replaceChildren();
+    //     gridContainer.replaceChildren();
     //     drawGrid()
     // };
     // window.addEventListener("resize", redrawGrid);
